29dcabaad9349673a13b72909cf00644d4c90cb5
arturocampi <arturo.herraez.tpz@gmail.com>
2021-10-07 18:49:28 +0200

    07/10

--- Ejercicios_Formularios/Ejercicio6/formularioSubidaArchivos.php
+++ Ejercicios_Formularios/Ejercicio6/formularioSubidaArchivos.php
@@ -1,6 +1,9 @@
 <form action="subida_archivos.php" method="post" enctype="multipart/form-data">
     Escoja dos ficheros<br>
+    <!--
     <input type="file" name="fichero1"> <br>
     <input type="file" name="fichero2"> <br>
+    -->
+    <input type="file" name="fichero[]"> <br>
     <input type="submit" value="subir fichero">
 </form>
--- Ejercicios_Formularios/Ejercicio6/subida_archivos.php
+++ Ejercicios_Formularios/Ejercicio6/subida_archivos.php
@@ -1,17 +1,21 @@
 <?php
+echo "Nombre del fichero. ".$_FILES["fichero1"]["name"]."<br>";
+echo "Nombre del fichero. ".$_FILES["fichero2"]["name"]."<br>";
 
-$name = $_FILES['fichero1']['name'];
-$size = $_FILES['fichero1']['size'];
-$name2 = $_FILES['fichero2']['name'];
-$size2 = $_FILES['fichero2']['size'];
-echo "Nombre del fichero. ".$_FILES["fichero1"]["name"];
-echo  '$name2'+'size2'+'<br>';
-
+/*
 $res = move_uploaded_file ($_FILES["fichero1"]["tmp_name"], "archivos_subidos/".$_FILES["fichero1"]["name"]);
 $res2 = move_uploaded_file ($_FILES["fichero2"]["tmp_name"], "archivos_subidos/".$_FILES["fichero2"]["name"]);
+*/
 
 if ($res && $res2) {
     echo "<br>Ficheros guardados";
 } else {
     echo "<br>Error";
+}
+
+$index_fichero = count($_FILES['fichero']['name']);
+var_dump($_FILES['fichero']);
+for ($i=1; $i < $index_fichero; $i++) { 
+    echo "Nombre del fichero. ".$_FILES["fichero[$i]"]["name"][$i]."<br>";
+    $res = move_uploaded_file ($_FILES["fichero"]["tmp_name"][$i], "archivos_subidos/".$_FILES["fichero"]["name"][$i]);
 }
