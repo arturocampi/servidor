85718867e3513a6c75443711f3dc004bf53b8572
arturocampi <arturo.herraez.tpz@gmail.com>
2021-11-05 12:34:13 +0100

    test

--- POO/Ejercicios/Ejercicio7/views/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <ul>
-        <li><a href="?method=fibonacci">Fibonacci</a></li>
-        <li><a href="?method=potencia">Potencia elevado a 2</a></li>
-        <li><a href="?method=factorial">Factoriales</a></li>
-        <li><a href="?method=primo">Numeros Primos</a></li>
-    </ul>
-</header>
--- POO/Ejercicios/Ejercicio8/views/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "Has iniciado sesión, bienvenido!";
-?>
-
-
-
-<form method="POST" action="../index.php?method=logout">
-    <input type="submit" value="log out">
-</form>
